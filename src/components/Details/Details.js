1b20a2ae48db0d27dfcb9c7377f44cc6e72b427d
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-09-30 16:40:01 +0600

    personal info added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Details.css'
+import React from "react";
+import "./Details.css";
 
 const Details = () => {
-    return (
-        <div>
-            <h1>mahmudul haque noor</h1>
+  return (
+    <div className="details-section">
+      <div className="personal-details">
+        <h1>Mahmudul Haque Noor</h1>
+        <p>
+          Email: <i>mahmudulhaquenoor@gmail.com</i>
+        </p>
+        <div className="extra-info">
+            <div>
+              <h2>45kg</h2>
+              <p>Weight</p>
+            </div>
+            <div>
+                <h2>5.5</h2>
+                <p>Height</p>
+            </div>
+            <div>
+                <h2>22years</h2>
+                <p>Age</p>
+            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Details;
